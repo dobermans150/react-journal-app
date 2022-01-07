e91bb12dccae88359a06264cced5811738571f5c
Christian <christianjguevara@hotmail.com>
2022-01-07 15:52:29 -0400

    stryles in NoteScreen

--- src/components/notes/NoteScreen.jsx
+++ src/components/notes/NoteScreen.jsx
@@ -5,6 +5,29 @@ export const NoteScreen = () => {
     return (
         <div className="notes__main-content">
             <NotesAppBar />
+
+            <div className="notes__content">
+                <input
+                    type="text"
+                    placeholder="Some awesome title"
+                    className="notes__title-input"
+                    autoComplete="off"
+                />
+
+                <textarea
+                    placeholder="What happened today"
+                    className="notes__textarea"
+                >
+
+                </textarea>
+
+                <div className="notes__image">
+                    <img
+                        alt="imagen"
+                        src="https://neliosoftware.com/es/wp-content/uploads/sites/3/2018/07/aziz-acharki-549137-unsplash-1200x775.jpg"
+                    />
+                </div>
+            </div>
         </div>
     )
 }
