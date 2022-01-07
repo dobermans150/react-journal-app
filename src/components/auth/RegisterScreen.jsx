d98f7939996f1d4a7f2cb46b413dfff52c8cbc08
Christian <christianjguevara@hotmail.com>
2022-01-07 11:02:31 -0400

    style journal page and auth pages

--- src/components/auth/RegisterScreen.jsx
+++ src/components/auth/RegisterScreen.jsx
@@ -1,10 +1,62 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export const RegisterScreen = () => {
     return (
-        <div>
-            <h1>Register Screen</h1>
-        </div>
+        <>
+            <h3 className="auth__title">Register</h3>
+
+            <form>
+
+                <input
+                    autoComplete='off'
+                    type="text"
+                    placeholder="Name"
+                    name="name"
+                    className="auth__input"
+                />
+                <input
+                    autoComplete='off'
+                    type="email"
+                    placeholder="Email"
+                    name="email"
+                    className="auth__input"
+                />
+
+                <input
+                    type="password"
+                    placeholder="Password"
+                    name="password"
+                    className="auth__input"
+
+                />
+                <input
+                    type="password"
+                    placeholder="Confirm Password"
+                    name="confirm"
+                    className="auth__input"
+
+                />
+
+                <button
+                    type="submit"
+                    className="btn btn-primary btn-block mb-5"
+                >
+                    Login
+                </button>
+
+
+
+                <Link
+                    to="/auth/login"
+                    className="link"
+                >
+                    Already registered?
+                </Link>
+            </form>
+
+
+        </>
     )
 
 }
---
+++ src/components/journal/JournalEntrie.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+
+export const JournalEntrie = () => {
+    return (
+        <div className="journal__entry">
+            <div
+                className="journal__entry-picture"
+                style={ {
+                    backgroundSize: 'cover',
+                    backgroundImage: 'url(https://neliosoftware.com/es/wp-content/uploads/sites/3/2018/07/aziz-acharki-549137-unsplash-1200x775.jpg)'
+                } }
+            >
+            </div>
+
+            <div className="journal__entry-body">
+                <p className="journal__entry-title">
+                    Un nuevo dia
+                </p>
+
+                <p className="journal__entry-content">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora cupiditate
+                </p>
+            </div>
+
+            <div className="journal__entry-date-box">
+                <span>Monday</span>
+                <h4>28</h4>
+            </div>
+        </div>
+    )
+}
